64c3c9cfaf39216dd7ea7d2c462c9eeebc4e7050
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-09-25 11:34:15 +0330

    fix antd config

--- src/component/Options.js
+++ src/component/Options.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Options.css";
+
 export default class Options extends React.Component {
-    
+
     render() {
         return (
             <div className="Options">
-                <h1>HELLO WORLD</h1>
+
             </div>
         );
     }

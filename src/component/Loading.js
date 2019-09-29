362aa929852c0022f0d7b84816167a508cbf6fb1
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-09-30 00:52:17 +0330

    loading sample inited

---
+++ src/component/Loading.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default class Loading extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div className="App">
+                <h1>Wait please</h1>
+            </div>
+        );
+    }
+}
